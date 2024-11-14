f142ba10bb49f43f04b5c3d7a0f9192ad983852f
22022112-PhamNgocKhanh <22022112@vnu.edu.vn>
2024-11-14 20:51:20 +0700

    lam lai github

--- README.md
+++
@@ -1,2 +0,0 @@
-# Bai_tap_lon
-đây là chỗ để ae cây khế làm bài tập lớn oop
--- src/LibraryApp.java
+++
@@ -1,9 +0,0 @@
-// LibraryApp.java
-import javax.swing.SwingUtilities;
-
-public class LibraryApp {
-    public static void main(String[] args) {
-        // Khởi chạy màn hình khóa
-        SwingUtilities.invokeLater(() -> new LockScreen().setVisible(true));
-    }
-}
